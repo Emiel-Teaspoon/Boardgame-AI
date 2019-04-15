f593f707d8792550e78a98363a494f77e753fd09
Leon Smit <leon_94@live.nl>
2019-04-15 11:06:41 +0200

    Start Scene fix

--- Application/src/client/scenes/StartScene.java
+++ Application/src/client/scenes/StartScene.java
@@ -1,19 +1,40 @@
 package client.scenes;
 
 //import com.sun.org.apache.xpath.internal.operations.String;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.*;
 import client.ClientModel;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.TextFlow;
 
 import java.util.HashMap;
 
 public class StartScene extends ClientScene {
 
     private Button btSettings;
+    private Button btPlay;
+
+    private BorderPane mainLayout;
+    private HBox settingsBox;
+    private TextFlow errorPane;
+    private GridPane centerGrid;
+    private HBox buttonLayout;
+
+    private Label nameLabel;
+    private TextField nameField;
+
+    public StartScene(ClientModel model) {
+        super(model);
+        buildScene();
+    }
 
     @Override
     public void onEnter() {
@@ -21,6 +42,7 @@ public class StartScene extends ClientScene {
             model.getConnection().close();
         }
         System.out.println("disconnected");
+        nameField.setText(model.getSettings().get("name"));
     }
 
     @Override
@@ -28,22 +50,10 @@ public class StartScene extends ClientScene {
 
     }
 
-    private Button btPlay;
-
-    private Label lbName;
-    private Label lbError;
-
-    private TextField tfName;
-
-    public StartScene(ClientModel model) {
-        super(model);
-        buildScene();
-    }
-
     private void play() {
-        if (!model.getSettings().get("name").equals(tfName.getText())) {
+        if (!model.getSettings().get("name").equals(nameField.getText())) {
             HashMap<String, String> newSetting = new HashMap<>();
-            newSetting.put("name", tfName.getText());
+            newSetting.put("name", nameField.getText());
             model.updateSettings(newSetting);
         }
 
@@ -58,15 +68,41 @@ public class StartScene extends ClientScene {
     @Override
     public void buildScene() {
         buildButtons();
-        buildLabels();
-        buildTextFields();
+        buildNodes();
+        buildLayoutManagers();
 
-        BorderPane pane = new BorderPane();
-        pane.setRight(btPlay);
-        pane.setLeft(btSettings);
-        pane.setTop(tfName);
+        scene = new Scene(mainLayout, 640, 480);
+    }
 
-        scene = new Scene(pane, 640, 480);
+    private void buildLayoutManagers() {
+        mainLayout = new BorderPane();
+        mainLayout.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+
+        settingsBox = new HBox();
+        settingsBox.setAlignment(Pos.CENTER_RIGHT);
+        settingsBox.getChildren().add(btSettings);
+        mainLayout.setTop(settingsBox);
+
+        centerGrid = new GridPane();
+        centerGrid.setAlignment(Pos.CENTER);
+        centerGrid.setHgap(10.0);
+        centerGrid.setVgap(12.0);
+
+        errorPane = new TextFlow();
+        errorPane.setTextAlignment(TextAlignment.LEFT);
+        centerGrid.add(errorPane, 0, 0, 3, 1);
+
+        centerGrid.add(nameLabel, 1, 1);
+        centerGrid.add(nameField, 2, 1);
+
+        buttonLayout = new HBox();
+        buttonLayout.setAlignment(Pos.CENTER);
+        buttonLayout.setSpacing(10.0);
+
+        buttonLayout.getChildren().add(btPlay);
+        centerGrid.add(buttonLayout, 1, 2, 2, 1);
+
+        mainLayout.setCenter(centerGrid);
     }
 
     private void buildButtons() {
@@ -81,16 +117,8 @@ public class StartScene extends ClientScene {
         });
     }
 
-    private void buildLabels() {
-        lbName = new Label("naam");
-        lbError = new Label("Errortext");
-        lbError.setTextFill(Color.CRIMSON);
+    private void buildNodes(){
+        nameField = new TextField();
+        nameLabel = new Label("Naam: ");
     }
-
-    private void buildTextFields() {
-        tfName = new TextField();
-        tfName.setText(model.getSettings().get("name"));
-    }
-
-
 }
